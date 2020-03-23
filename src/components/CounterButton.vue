a0a480e01c6ba22c86d23b6c29e6f86904f1cf21
Tan Tian Le <42717379+Tian-Le97@users.noreply.github.com>
2020-03-23 11:10:02 +0800

    Renewed Grocery File

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button v-on:click='decrement()'>-</button>
+        <label>&nbsp;{{counter}}&nbsp;</label>
+        <button v-on:click='increment()'>+</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            counter:0
+        }
+    },
+    methods:{
+        decrement(){
+            if(this.counter>0) {
+                this.counter = this.counter - 1
+                return this.counter;
+            } else
+                return this.counter;
+        },
+        increment(){
+            this.counter = this.counter + 1;
+            return this.counter;
+        }
+    }
+}
+</script>
